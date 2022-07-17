99e5b80e0204769a3ca3fb0eacebdfe69455f08b
ssioco <juanjo.9912@gmail.com>
2022-07-16 23:27:54 -0500

    ApiCrudv2

--- api.js
+++ api.js
@@ -1,18 +1,64 @@
 const div = document.querySelector('.Contenedor');
-const valor = document.querySelector('#valor')
+const valor = document.querySelector('#valor');
+const boton = document.querySelector('#botoncito');
+const boton2 = document.querySelector('#botoncito2');
+const boton3 = document.querySelector('#botoncito3');
 
-function mostrarProducto(i) {
-    fetch(`https://fakestoreapi.com/products/${i}`)
+
+boton.addEventListener('click',(e) =>{
+    e.preventDefault();
+    mostrarProducto(valor.value);
+})
+
+boton2.addEventListener('click',(e)=>{
+    e.preventDefault();
+    actualizarProducto();
+})
+
+boton3.addEventListener('click',(e)=>{
+    e.preventDefault();
+    eliminarProducto();
+})
+
+function mostrarProducto(valor) {
+    fetch(`https://fakestoreapi.com/products/${valor}`)
         .then((res)=>res.json())
         .then((data) => {
             crearProducto(data);
         })
     }
 
-function mostrarProductos(valor) {
+function mostrarProductos(valor){
     for (let i=1;i<=valor;i++){
         mostrarProducto(i)
     }
+}
+
+//titulo,precio,descripcion,imagen,categoria
+
+function actualizarProducto() {
+    fetch('https://fakestoreapi.com/products/7',{
+            method:"PUT",
+            body:JSON.stringify(
+                {
+                    title: 'test product',
+                    price: 13.5,
+                    description: 'lorem ipsum set',
+                    image: 'https://i.pravatar.cc',
+                    category: 'electronic'
+                }
+            )
+        })
+            .then(res=>res.json())
+            .then(json=>console.log(json))   
+}
+
+function eliminarProducto() {
+    fetch('https://fakestoreapi.com/products/5',{
+            method:"DELETE"
+        })
+            .then(res=>res.json())
+            .then(json=>console.log(json))
 }
 
 function crearProducto(producto) {
@@ -28,4 +74,4 @@ function crearProducto(producto) {
 </div>`)
     div.appendChild(div_2)
     }
-mostrarProductos(20);
+//mostrarProductos(20);
